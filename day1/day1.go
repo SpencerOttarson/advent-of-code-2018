dccbdfc55db08cc89752c6a022074a0447d2446a
Spencer Ottarson <ottarso5@msu.edu>
2018-12-17 13:55:58 -0500

    Day 1

--- day1/day1.go
+++ day1/day1.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+
     file, err := os.Open("input1.txt")
     if err != nil {
         log.Fatal(err)
@@ -17,16 +18,54 @@ func main() {
 
     scanner := bufio.NewScanner(file)
     total := 0
+
+    var values []int
+
+    var firstDuplicate int = 0
+    firstDuplicateFound := false
+    counts := make(map[int]int)
+    counts[0] = 1
+
     for scanner.Scan() {
         value, err := strconv.Atoi(scanner.Text())
         if err != nil {
             log.Fatal(err)
         }
 
+        values = append(values, value)
+
         total += value
+
+        value, ok := counts[total]
+        if !ok {
+            counts[total] = 1
+        } else {
+            if value >= 1 && firstDuplicateFound == false {
+                firstDuplicate = total
+                firstDuplicateFound = true
+            }
+            counts[total] = value + 1
+        }
     }
 
     fmt.Println(total)
+
+    for i := 0; firstDuplicateFound == false; i = (i + 1) % len(values) {
+        total += values[i]
+
+        value, ok := counts[total]
+        if !ok {
+            counts[total] = 1
+        } else {
+            if value >= 1 && firstDuplicateFound == false {
+                firstDuplicate = total
+                firstDuplicateFound = true
+            }
+            counts[total] = value + 1
+        }
+    } 
+    
+    fmt.Println(firstDuplicate)
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
